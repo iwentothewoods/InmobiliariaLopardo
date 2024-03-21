38efc2ad58c81066a80f80dea79ddc2d2cdef723
Luna Lopardo <luna.lopardo@gmail.com>
2024-03-21 17:21:25 -0300

    Propietario ABM

--- inmobiliariaLopardo/Controllers/PropietariosController.cs
+++ inmobiliariaLopardo/Controllers/PropietariosController.cs
@@ -22,6 +22,108 @@ public class PropietariosController : Controller
         return View(lista);
     }
 
+    /*public IActionResult Editar(int id)
+    {
+        if (id > 0)
+        {
+            RepositorioPropietarios rp = new RepositorioPropietarios();
+            var propietario = rp.GetPropietario(id);
+            return View(propietario);
+        }
+        else
+        {
+            return View();
+        }
+    }*/
+
+    /*public IActionResult Guardar(Propietario propietario)
+    {
+        RepositorioPropietarios rp = new RepositorioPropietarios();
+        rp.GuardarPropietario(propietario);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Modificar(Propietario propietario)
+    {
+        RepositorioPropietarios rp = new RepositorioPropietarios();
+        rp.ModificarPropietario(propietario);
+        return RedirectToAction(nameof(Index));
+    }*/
+
+     // GET: Propietarios/Crear
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        // POST: Propietarios/Crear
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Crear(Propietario p)
+        {
+            try
+            {
+                RepositorioPropietarios repo = new RepositorioPropietarios();
+                repo.Alta(p);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Propietarios/Edit/5
+        public ActionResult Editar(int id)
+        {
+            RepositorioPropietarios repo = new RepositorioPropietarios();
+            var propietario = repo.GetPropietario(id);
+            return View(propietario);
+        }
+
+
+        // POST: Propietarios/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(int id, Propietario p)
+        {
+            try
+            {
+                RepositorioPropietarios repo = new RepositorioPropietarios();
+                repo.Modificacion(p);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Propietarios/Delete/5
+        public ActionResult Eliminar(int id)
+        {
+            RepositorioPropietarios repo = new RepositorioPropietarios();
+            var propietario = repo.GetPropietario(id);
+            return View(propietario);
+        }
+
+        // POST: Propietarios/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Eliminar(int id, Propietario p)
+        {
+            try
+            {
+                RepositorioPropietarios repo = new RepositorioPropietarios();
+                repo.Baja(id);
+                TempData["Mensaje"] = "Eliminación realizada correctamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
 }
 
 
--- inmobiliariaLopardo/Models/RepositorioPropietarios.cs
+++ inmobiliariaLopardo/Models/RepositorioPropietarios.cs
@@ -1,6 +1,8 @@
 using MySql.Data.MySqlClient;
 using System.Data;
 namespace inmobiliariaLopardo.Models;
+
+using System;
 using Microsoft.Extensions.Configuration;
 
 
@@ -10,9 +12,10 @@ public class RepositorioPropietarios
 
     public RepositorioPropietarios()
     {
-        
+
     }
 
+    //Listar Propietarios
     public IList<Propietario> GetPropietarios()
     {
         var propietarios = new List<Propietario>();
@@ -43,4 +46,112 @@ public class RepositorioPropietarios
         return propietarios;
     }
 
+    public Propietario? GetPropietario(int id)
+    {
+        Propietario? propietario = null;
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            var sql = $"SELECT {nameof(Propietario.Id)}, {nameof(Propietario.Nombre)}, {nameof(Propietario.Apellido)}, {nameof(Propietario.Email)}, {nameof(Propietario.Dni)}, {nameof(Propietario.Telefono)} FROM propietarios WHERE {nameof(Propietario.Id)} = @id";
+            using (var command = new MySqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@id", id);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        propietario = new Propietario
+                        {
+                            Id = reader.GetInt32(nameof(Propietario.Id)),
+                            Nombre = reader.GetString(nameof(Propietario.Nombre)),
+                            Apellido = reader.GetString(nameof(Propietario.Apellido)),
+                            Email = reader.GetString(nameof(Propietario.Email)),
+                            Telefono = reader.GetString(nameof(Propietario.Telefono)),
+                            Dni = reader.GetString(nameof(Propietario.Dni))
+
+                        };
+                    }
+                }
+            }
+        }
+        return propietario;
+    }
+
+
+    //Crear Propietario
+    public int Alta(Propietario p)
+    {
+        int res = -1;
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            var sql = @$"INSERT INTO propietarios ({nameof(Propietario.Nombre)}, {nameof(Propietario.Apellido)}, {nameof(Propietario.Dni)}, 
+            {nameof(Propietario.Email)}, {nameof(Propietario.Telefono)}) VALUES (@{nameof(Propietario.Nombre)}, @{nameof(Propietario.Apellido)}, 
+            @{nameof(Propietario.Dni)}, @{nameof(Propietario.Email)}, @{nameof(Propietario.Telefono)}); SELECT LAST_INSERT_ID();";
+            
+            using (var command = new MySqlCommand(sql, connection))
+            {
+                command.CommandType = CommandType.Text;
+                command.Parameters.AddWithValue("@nombre", p.Nombre);
+                command.Parameters.AddWithValue("@apellido", p.Apellido);
+                command.Parameters.AddWithValue("@dni", p.Dni);
+                command.Parameters.AddWithValue("@telefono", p.Telefono);
+                command.Parameters.AddWithValue("@email", p.Email);
+
+                connection.Open();
+                res = Convert.ToInt32(command.ExecuteScalar());
+                p.Id = res;
+                connection.Close();
+            }
+        }
+        return res;
+    }
+
+    //Eliminar Propietario
+    public bool Baja(int propietarioId)
+    {
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            string sql = "DELETE FROM propietarios WHERE Id = @id";
+            using (var command = new MySqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@id", propietarioId);
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                connection.Close();
+
+                return rowsAffected > 0;
+            }
+        }
+    }
+
+    //Modificar Propietario
+    public bool Modificacion(Propietario p)
+    {
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            string sql = @"UPDATE propietarios SET 
+                      Nombre = @nombre,
+                      Apellido = @apellido,
+                      Dni = @dni,
+                      Email = @email,
+                      Telefono = @telefono
+                      WHERE Id = @id";
+
+            using (var command = new MySqlCommand(sql, connection))
+            {
+                command.Parameters.AddWithValue("@nombre", p.Nombre);
+                command.Parameters.AddWithValue("@apellido", p.Apellido);
+                command.Parameters.AddWithValue("@dni", p.Dni);
+                command.Parameters.AddWithValue("@telefono", p.Telefono);
+                command.Parameters.AddWithValue("@email", p.Email);
+                command.Parameters.AddWithValue("@id", p.Id);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                connection.Close();
+
+                return rowsAffected > 0;
+            }
+        }
+    }
 }
